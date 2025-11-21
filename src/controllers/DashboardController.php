9f6ef877bd168e011a5fc3fae20e79227b769c68
Paweł Buczek <151944237+Buczy8@users.noreply.github.com>
2025-11-21 12:56:46 +0100

    lab06

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 class DashboardController extends AppController {
 
     public function index(){
@@ -42,6 +43,11 @@ class DashboardController extends AppController {
                 'href' => '/cards/ten-of-hearts'
             ],
         ];
+
+        $userRepository = new UserRepository();
+        $users = $userRepository->getUser();
+
+        var_dump($users);
 
         return $this->render('dashboard', ['cards' => $cards]);
     }
